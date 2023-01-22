dab359600a36b68416fb3eac407afeab007ecae6
Davi <davi_martins2009@outlook.com>
2023-01-21 22:05:29 -0300

    Comandos de entrada

---
+++ Course JavaScript/test.js
@@ -0,0 +1,8 @@
+// PEDE PARA DIGITAR SEU NOME E SOBRENOME NO NAVEGADOR
+var nome = prompt('Qual seu nnome?')
+var sobrenome = prompt('Qual seu sobrenome?')
+console.log('Olá ' + nome + ' ' + sobrenome)
+
+document.write("<h1>Bem Vindo ao projeto " + nome + "</h1>")
+
+document.write("<img src='https://sujeitoprogramador.com/steve.png' alt='Foto do Steve' />")
