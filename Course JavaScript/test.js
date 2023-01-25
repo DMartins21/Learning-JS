897a1ccd0de8df43086f13bc5c4d132ed9adb21b
Davi <davi_martins2009@outlook.com>
2023-01-25 14:06:18 -0300

    Calculando IMC

--- Course JavaScript/test.js
+++ Course JavaScript/test.js
@@ -1,23 +1,35 @@
-// Switch
+var peso
+var altura
+var imc
+var resultado
+//event define comportamento
 
-function pedir(){
-    var valor = prompt('Digite um valor de 1 a 4')
+function calcular(event){
+    event.preventDefault()
 
-    switch(Number(valor)){
-        case 1:
-            alert('Você escolheu 1 - Suco')
-            break
-        case 2:
-            alert('Você escolheu 2 - Água')
-            break
-        case 3:
-            alert('Você escolheu 3 - Sorvete')
-            break
-        case 4:
-            alert('Você chamou o garçom')
-            break
-        default:
-            alert('Escolha de 1 a 4')
-            break
+    peso = document.getElementById('peso').value
+    altura = document.getElementById('altura').value
+
+    imc = peso / (altura * altura)
+
+    resultado = document.getElementById('resultado')
+
+    if(imc < 17){
+        resultado.innerHTML = '<br> Seu resultado foi..' + imc + '<br> Você está muito abaixo do peso'
     }
+    else if(imc > 17 && imc <=18.49){
+        resultado.innerHTML = '<br> Seu resultado foi..' + imc + '<br> Você está abaixo do peso'
+    }
+    else if(imc >= 18.5 && imc < 24.99){
+        resultado.innerHTML = '<br> Seu resultado foi..' + imc + '<br> Você está no peso ideal'
+    }
+    else if(imc > 25 && imc <= 29.99){
+        resultado.innerHTML = '<br> Seu resultado foi..' + imc + '<br> Você está acima do peso'
+    }
+    else if(imc >= 30){
+        resultado.innerHTML = '<br> Seu resultado foi..' + imc + '<br> Cuidado, Obesidade!'
+    }
+
+    document.getElementById('peso').value = ''
+    document.getElementById('altura').value = ''
 }
