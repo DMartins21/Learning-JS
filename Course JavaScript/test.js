f58bb7d75b0052c31338d55451b49bb460b8b39a
Davi <davi_martins2009@outlook.com>
2023-01-30 10:52:33 -0300

    Temporizadores

--- Course JavaScript/test.js
+++ Course JavaScript/test.js
@@ -1,35 +1,13 @@
-var peso
-var altura
-var imc
-var resultado
-//event define comportamento
+//setInterval
 
-function calcular(event){
-    event.preventDefault()
-
-    peso = document.getElementById('peso').value
-    altura = document.getElementById('altura').value
-
-    imc = peso / (altura * altura)
-
-    resultado = document.getElementById('resultado')
-
-    if(imc < 17){
-        resultado.innerHTML = '<br> Seu resultado foi..' + imc + '<br> Você está muito abaixo do peso'
-    }
-    else if(imc > 17 && imc <=18.49){
-        resultado.innerHTML = '<br> Seu resultado foi..' + imc + '<br> Você está abaixo do peso'
-    }
-    else if(imc >= 18.5 && imc < 24.99){
-        resultado.innerHTML = '<br> Seu resultado foi..' + imc + '<br> Você está no peso ideal'
-    }
-    else if(imc > 25 && imc <= 29.99){
-        resultado.innerHTML = '<br> Seu resultado foi..' + imc + '<br> Você está acima do peso'
-    }
-    else if(imc >= 30){
-        resultado.innerHTML = '<br> Seu resultado foi..' + imc + '<br> Cuidado, Obesidade!'
-    }
-
-    document.getElementById('peso').value = ''
-    document.getElementById('altura').value = ''
+function acao(){
+    document.write("Executando... <br>")
 }
+
+/*var time = setInterval(() => {
+    document.write("Executando!!!!!... <br>")
+},1000)*/
+
+setTimeout(() => {
+    document.write("Executando. <br>")
+}, 3000)
